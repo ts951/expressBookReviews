fe20b96b93daecde51c1069a55e896e93a2e06a0
Thomas Squires <thomassquires951@gmail.com>
2025-06-24 10:04:00 -0400

    Add user authentication code

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,28 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    // Check if user is logged in and has a valid token
+    if(req.session.authorisation) {
+        // Get the JWT token
+        let token = req.session.authorisation['accessToken'];
+
+        // Verify JWT
+        jwt.verify(token, "access", (err, user) => {
+            if(!err) {
+                req.user = user;
+                next(); // Go to next middleware
+            }
+            else {
+                // If JWT not valid, display error message
+                return res.status(403).json({message: "User not authenticated."});
+            }
+        });
+    }
+
+    else {
+        // If user is not logged in
+        return res.status(403).json({message: "User not logged in"});
+    }
 });
  
 const PORT =5000;
